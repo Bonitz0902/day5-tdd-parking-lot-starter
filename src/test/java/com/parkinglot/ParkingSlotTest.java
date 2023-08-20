3cdc95a37a3c47cc909f041476dcfbeee4263fa5
Guile <jimenezguiles7255@gmail.com>
2023-08-20 20:00:20 +0800

    TEST: Created a test that will satisfy all the Cases given on the story 1

--- src/test/java/com/parkinglot/ParkingSlotTest.java
+++ src/test/java/com/parkinglot/ParkingSlotTest.java
@@ -1,17 +1,76 @@
 package com.parkinglot;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingSlotTest {
+    ParkingLot parkingLot = new ParkingLot(10);
     @Test
-    public void should_return_ticket_when_execute_getParkingTicket_given_car(){
-        ParkingLot parkingLot = new ParkingLot(10);
-        ParkingSlot parkingTicket = parkingLot.parkVehicle();
+    public void should_return_parking_ticket_when_execute_parkCar_given_parking_lot_and_car(){
+        Car car = new Car("123ABC");
+        ParkingTicket ticket = parkingLot.parkCar(car);
 
-        assertNotNull(parkingTicket);
+
+        assertNotNull(ticket);
+
+    }
+
+    @Test
+    public void should_return_parked_car_when_execute_retrieve_car_given_parking_lot_with_parked_car_and_ticket(){
+        Car car = new Car("123ABC");
+        ParkingTicket ticket = parkingLot.parkCar(car);
+
+        Car retreivedCar = parkingLot.retrieveCar(ticket);
+
+        assertEquals(car, retreivedCar);
+    }
+
+    @Test
+    public void should_return_right_car_each_ticket_when_execute_retrieve_car_given_2_parked_cars_and_2_parking_tickets(){
+        Car car1 = new Car("123123");
+        Car car2 = new Car("ABCABC");
+
+        ParkingTicket ticket1 = parkingLot.parkCar(car1);
+        ParkingTicket ticket2 = parkingLot.parkCar(car2);
+
+        Car retrievedCar1 = parkingLot.retrieveCar(ticket1);
+        Car retrievedCar2 = parkingLot.retrieveCar(ticket2);
+
+        assertEquals(car1,retrievedCar1);
+        assertEquals(car2,retrievedCar2);
+
+    }
+
+    @Test
+    public void should_return_null_car_when_retrieve_car_given_parking_lot_and_a_wrong_ticket(){
+        ParkingTicket wrongTicket = new ParkingTicket(null);
+
+        Car retrievedCar = parkingLot.retrieveCar(wrongTicket);
+
+        assertNull(retrievedCar);
+    }
+
+    @Test
+    public void should_return_null_car_when_execute_retrieveCar_given_parking_lot_and_used_parking_ticket(){
+        Car car1 = new Car("AAAAA");
+
+        ParkingTicket parkingTicket1 = parkingLot.parkCar(car1);
+        ParkingTicket parkingTicket2 = parkingLot.parkCar(car1);
+
+        Car retrivedCar = parkingLot.retrieveCar(parkingTicket1);
+        Car nothingRetrivedCar = parkingLot.retrieveCar(parkingTicket1);
+
+        assertNull(nothingRetrivedCar);
+    }
+
+    @Test
+    public void should_return_nothing_when_retrived_car_given_null_parking_lot_and_parking_ticket(){
+        ParkingLot nullParkingLot = new ParkingLot(0);
+        Car car1 = new Car("AAAAA");
+        ParkingTicket parkingTicket = nullParkingLot.parkCar(car1);
+
+        assertNull(parkingTicket);
 
     }
 
